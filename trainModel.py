c841f24dc4d2f9951851a9955e637bfdb3644293
massimoalbarello <albo.max@hotmail.it>
2021-12-01 18:11:24 +0100

    store models

--- trainModel.py
+++ trainModel.py
@@ -2,6 +2,7 @@ from preprocessing import importDataset
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+import pickle
 
 
 
@@ -9,10 +10,11 @@ datasetName = "dataset.csv"
 
 features, labels, scaler = importDataset(datasetName)
 
-models = [("support vector", svm.SVC(kernel='linear', C=1, random_state=0)), ("random forest", RandomForestClassifier(max_depth=50, random_state=0))]
+models = [("support_vector", svm.SVC(kernel='linear', C=1, random_state=0)), ("random_forest", RandomForestClassifier(max_depth=5, random_state=0))]
 
-for modelName, clf in models:  
+for modelName, clf in models:
     scores = cross_val_score(clf, features, labels, cv=5)
     print("\n" + modelName + ": %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+    pickle.dump(clf, open("./omnia/models/" + modelName + ".sav", 'wb'))
 
 print()
