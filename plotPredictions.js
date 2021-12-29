4020904a116db8476d1a10c67ac06741a8b5c83c
massimoalbarello <massimo00albarello@gmail.com>
2021-12-29 17:40:08 +0100

    plot daily accuracies

--- plotPredictions.js
+++ plotPredictions.js
@@ -29,12 +29,39 @@ function plotFunction(predictions, correctStates, timestamps, title) {
 
 async function main() {
     const [predictions, correctStates, predictionTimestamps] = await influxQuery.getPredictions(predictionsDBname, "");
-    var dates = [];
-    for (const predictionTimestamp of predictionTimestamps) {
-        dates.push(new Date(predictionTimestamp));
-    }
-    plotFunction(predictions, correctStates, dates, "Predictions and correct states plot");
+    
+    var lastDate = new Date(predictionTimestamps[0])
+    var lastDay = lastDate.getDate();
+    var predictionsDate = [];
+    var accuraciesDates = [];
+    var accs = [];
+    var tot = 1;
+    var count = predictions[0] == correctStates[0] ? 1 : 0;
+    var i = 1;
 
+    for (const predictionTimestamp of predictionTimestamps.slice(1)) {
+        const date = new Date(predictionTimestamp);
+        if (date.getDate() == lastDay) {
+            tot += 1;
+            count += predictions[i] == correctStates[i] ? 1 : 0;
+        }
+        else {
+            accs.push(count/tot);
+            accuraciesDates.push(lastDate);
+            console.log("[" + lastDate.getDate() + "/" + lastDate.getMonth() + "]: " + count + "/" + tot + " => " + count/tot);
+            lastDate = date;
+            lastDay = lastDate.getDate();
+            tot = 1;
+            count = predictions[i] == correctStates[i] ? 1 : 0;
+        }
+        i += 1;
+        predictionsDate.push(date);
+    }
+    accs.push(count/tot);
+    accuraciesDates.push(lastDate);
+    console.log("[" + lastDate.getDate() + "/" + lastDate.getMonth() + "]: " + count + "/" + tot + " => " + count/tot);
+    plotFunction(predictions, correctStates, predictionsDate, "Predictions and correct states plot");
+    plotFunction(accs, [], accuraciesDates, "Daily accuracy of predictions");
 }
 
 main();
